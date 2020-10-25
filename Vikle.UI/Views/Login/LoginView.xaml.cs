90793182101bc241f06187eb97f0ba3729bd2b89
David Fernández Ramos <dfernandez@fluendo.com>
2020-10-25 16:37:42 +0100

    Add Login view

---
+++ Vikle.UI/Views/Login/LoginView.xaml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vikle.UI.Views.Login
+{
+    /// <summary>
+    /// This class contains the definition of the login view.
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
