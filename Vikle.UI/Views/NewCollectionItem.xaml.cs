f8b6e6bc3816814808402874a96d8fe8713e7e64
David Fernández Ramos <dfernandez@fluendo.com>
2020-11-01 17:46:01 +0100

    Create NewCollectionItem template

---
+++ Vikle.UI/Views/NewCollectionItem.xaml.cs
@@ -0,0 +1,13 @@
+namespace Vikle.UI.Views
+{
+    /// <summary>
+    /// This class contains the definition of the new item option template for the CollectionView.
+    /// </summary>
+    public partial class NewCollectionItem
+    {
+        public NewCollectionItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
