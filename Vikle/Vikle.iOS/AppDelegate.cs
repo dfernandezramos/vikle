2492e9383a2f4de1b8c1e2b29439ee24364cac96
David Fernández Ramos <dfernandez@fluendo.com>
2020-11-08 10:02:01 +0100

    Remove unused files

--- Vikle/Vikle.iOS/AppDelegate.cs
+++ Vikle/Vikle.iOS/AppDelegate.cs
@@ -20,9 +20,6 @@ namespace Vikle.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
-
             return base.FinishedLaunching(app, options);
         }
     }
