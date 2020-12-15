8745104dfd7c7d0d3b95e82e4ca9fe8edd07ff01
David Fernández Ramos <dfz.ramos@gmail.com>
2020-12-15 18:38:53 +0100

    Add GNU license to all classes

--- Vikle/Vikle.iOS/AppDelegate.cs
+++ Vikle/Vikle.iOS/AppDelegate.cs
@@ -1,4 +1,11 @@
-﻿using Foundation;
+﻿// Copyright (C)  2020  David Fernández Ramos.
+// Permission is granted to copy, distribute and/or modify this document
+// under the terms of the GNU Free Documentation License, Version 1.3
+// or any later version published by the Free Software Foundation;
+// with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+// A copy of the license is included in the section entitled "GNU
+// Free Documentation License".
+using Foundation;
 using MvvmCross.Forms.Platforms.Ios.Core;
 using UIKit;
 using Vikle.UI;
